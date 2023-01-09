54d781e299018a5e3b64fbd41b87ef318738b86e
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-01-09 11:51:00 +0000

    Add errno from v to z and missing

--- src/waitid.s
+++ src/waitid.s
@@ -4,5 +4,11 @@
 waitid:
   movq $247, %rax
   syscall
+  cmpq $0, %rdi
+  jne .set_errno
+  ret
+
+.set_errno:
+  movq %rdi, errno(%rip)
   ret
 
--- src/writev.s
+++ src/writev.s
@@ -4,5 +4,11 @@
 writev:
   movq $20, %rax
   syscall
+  cmpq $0, %rdi
+  jne .set_errno
+  ret
+
+.set_errno:
+  movq %rdi, errno(%rip)
   ret
 
